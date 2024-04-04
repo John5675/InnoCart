e227e6274050055a2fff28e9e7dede39e9f6be47
jhuynh <79059047+John5675@users.noreply.github.com>
2024-04-03 22:46:09 -0700

    Category Implementation
    Implemented Receiving DummyJSON requests

--- innocart/src/App.js
+++ innocart/src/App.js
@@ -10,13 +10,18 @@ import {
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Footer from "./components/Footer/Footer";
+import store from "./store/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <Sidebar />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }

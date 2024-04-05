fcc63e90811625f94a244d073e427cd4728417a0
jhuynh <79059047+John5675@users.noreply.github.com>
2024-04-04 17:18:43 -0700

    Start of Footer

--- innocart/src/components/Footer/Footer.js
+++ innocart/src/components/Footer/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Footer.scss";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer bg-orange">
+      <div className="container py-4 text-center">
+        <div className="flex align-center justify-center text-white fw-3 fs-14">
+          <Link to="/" className="text-uppercase">
+            privacy policy
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
